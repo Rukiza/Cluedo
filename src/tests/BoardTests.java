d4494ef04dd2fd4c775f4ed5c8cc2af38675127b
Rukiza <theproros@gmail.com>
2014-08-14 04:43:31 +1200

    Started working on a test case need to finish later

--- src/tests/BoardTests.java
+++ src/tests/BoardTests.java
@@ -1,5 +1,23 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import cludo.gui.CludoBoard;
+
 public class BoardTests {
+
+	@Test
+	public void doorTest1() {
+		fail("Not yet implemented");
+	}
+	
+	
+	private CludoBoard makeBoard(File file){
+		return new CludoBoard(file);
+	}
 
 }
