659f7796583980ad008cab70733861f72082f957
Rukiza <theproros@gmail.com>
2014-08-14 07:39:47 +1200

    Move Changes: 	Added java doc to the move method explaning what it returns and takes as parameters

--- src/cludo/util/Move.java
+++ src/cludo/util/Move.java
@@ -14,6 +14,14 @@ public class Move {
 		this.board = board;
 	}
 	
+	/**
+	 * takes the old location of the player the new one and the player and will tell 
+	 * player who called it id they can move or not and handle movement in and out of rooms.
+	 * @param oldLocation - the location that the player is.
+	 * @param newLocation - the location the player wishes to move.
+	 * @param player - the player that is the player calling the method.
+	 * @return - returns true if the player can move to this point, false otherwise.
+	 */
 	public static boolean makeMove(Location oldLocation, Location newLocation, Player player){
 		int playerMoves = player.getCurrentMove();
 		List<Location> playersAttemptedMove = player.currentPath;
