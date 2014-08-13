765617f01d17b1efa9201eff94a8c6123d1a336f
Rukiza <theproros@gmail.com>
2014-08-14 07:21:42 +1200

    Changes to BoardTests: 	Testing for the boards is door methods 	Testing for is room methods

--- src/tests/BoardTests.java
+++ src/tests/BoardTests.java
@@ -7,17 +7,47 @@ import java.io.File;
 import org.junit.Test;
 
 import cludo.gui.CludoBoard;
+import cludo.util.Location;
 
 public class BoardTests {
+	
+	private String filePath = "TestBoards/";
 
 	@Test
 	public void doorTest1() {
-		fail("Not yet implemented");
+		CludoBoard board = makeBoard(filePath+"doorTest.txt");
+		assertTrue(board.isDoor(new Location(0, 0)));
+	}
+	
+	@Test
+	public void doorTest2()	{
+		CludoBoard board = makeBoard(filePath+"doorTest.txt");
+		for (int x = 0; x < board.getWidth(); x++){
+			for (int y = 0; y < board.getHeight(); y++){
+				if (!board.isDoor(new Location(0, 0))){
+					fail("The board is filled with doors");
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void roomTest1(){
+		CludoBoard board = makeBoard(filePath+ "roomTest.txt");
+		for (int x = 0; x < board.getWidth(); x++){
+			for (int y = 0; y < board.getHeight(); y++){
+				if (board.isDoor(new Location(0, 0))){
+				}else if (board.isRoom(new Location(x, y))){
+				}else {
+					fail("The board is filled with doors");
+				}
+			}
+		}
 	}
 	
 	
-	private CludoBoard makeBoard(File file){
-		return new CludoBoard(file);
+	private CludoBoard makeBoard(String fileName){
+		return new CludoBoard(new File(fileName));
 	}
 
 }
